55a58d9546adb49b625252e0d245f9d2ce09e930
vcshing <vcshing2005@hotmail.com>
2018-01-21 23:14:52 +0800

    asdsad

--- www/page/question/js/bindEvent.js
+++ www/page/question/js/bindEvent.js
@@ -19,7 +19,26 @@ function bindansConfirm(page){
 }
 
 function bindStatistics(page){
+  var chartArr = [];
+  chartArr["result"] = [];
 
-    //    app.view.current.router.navigate("/statistics/?id="+ page.detail.route.query.id,{reloadCurrent: false})
+  google.charts.load('current', {'packages':['corechart']});
+
+  ajaxGetStatisticsDetail({
+      "id": page.detail.route.query.id
+  }, function(response) {
+  //debugger;
+      chartArr["result"][0] = ["Task", "Result"]
+      $(response.result).each(function(index, result) {
+        chartArr["result"].push([result.answer, result.choiceCount])
+      })
+      //debugger;
+      if(chartArr["result"][1][0] == null){
+        chartArr["result"][1] = ["No Result", 1];
+      }
+      chartArr["title"] = response.result[0].title
+      chartArr["question"] = response.result[0].question
+      google.charts.setOnLoadCallback(function() { drawChart(chartArr); });
+  })
 
 }
