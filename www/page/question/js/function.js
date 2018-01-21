3ea923e871aa77343de2bbe9579a17c3b2c9acdc
vcshing <vcshing2005@hotmail.com>
2018-01-22 00:20:13 +0800

    zXasdas

--- www/page/question/js/function.js
+++ www/page/question/js/function.js
@@ -14,6 +14,30 @@ function renderQuestion(array) {
     return html
 }
 
+function genStatistics(page){
+  var chartArr = [];
+  chartArr["result"] = [];
+
+  google.charts.load('current', {'packages':['corechart']});
+
+  ajaxGetStatisticsDetail({
+      "id": page.detail.route.query.id
+  }, function(response) {
+  //debugger;
+      chartArr["result"][0] = ["Task", "Result"]
+      $(response.result).each(function(index, result) {
+        chartArr["result"].push([result.answer, result.choiceCount])
+      })
+      //debugger;
+      if(chartArr["result"][1][0] == null){
+        chartArr["result"][1] = ["No Result", 1];
+      }
+      chartArr["title"] = response.result[0].title
+      chartArr["question"] = response.result[0].question
+      google.charts.setOnLoadCallback(function() { drawChart(chartArr); });
+  })
+
+}
 
 function drawChart(chartArr) {
 
@@ -24,12 +48,12 @@ function drawChart(chartArr) {
     var options = {
 
         legend: {
-            position: "bottom"
+            //position: "bottom"
         },
         chartArea: {
-          width:"200%"
+
         },
-        pieStartAngle: 100
+        pieStartAngle: 200
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
